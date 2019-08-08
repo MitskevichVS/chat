30fc8d4758d26ad5ac2c539dc43c1b74522a547d
Mitskevich Victor <mv2.mx@yandex.ru>
2019-08-08 11:25:36 +0300

    feat: add login page

---
+++ src/components/login/login.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+
+import TextField from '@material-ui/core/TextField';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import Box from '@material-ui/core/Box';
+
+
+class Login extends Component {
+  render() {
+    return (
+      <Box
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        justifyContent="center"
+        p={1}
+        m={1}
+        style={{ height: '70vh' }}
+        
+      >
+        <Box
+        >
+          <Typography variant="h5">
+            Please, log in.
+          </Typography>
+        </Box>
+        <Box
+        >
+          <TextField
+            id="filled-with-placeholder"
+            label="Enter your name"
+            placeholder="Name"
+            // className={classes.textField}
+            margin="normal"
+            variant="filled"
+          />
+        </Box>
+        <Box
+        >
+          <Button variant="contained">
+            Submit
+          </Button>
+        </Box>
+      </Box>
+    )
+  }
+};
+
+export default Login;
