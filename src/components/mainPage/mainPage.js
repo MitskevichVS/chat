5355b76b070878e679499544911499a4c39783da
Mitskevich Victor <mv2.mx@yandex.ru>
2019-08-12 18:42:56 +0300

    feat: add simple notification

--- src/components/mainPage/mainPage.js
+++ src/components/mainPage/mainPage.js
@@ -4,22 +4,46 @@ import ReconnectingWebSocket from 'reconnecting-websocket';
 import Header from '../header/header';
 import ChatList from '../chatList/chatList';
 import ChatInput from '../chatInput/chatInput';
+import Snackbar from '@material-ui/core/Snackbar';
+import IconButton from '@material-ui/core/IconButton';
 
 
 class MainPage extends Component {
   state = {
-    messages: []
+    messages: [],
+    showNotification: false,
+    notificationMessageFrom: '',
+    displayChat: false,
+    connectionFlag: true
   }
-
-  // WebSocket = require('isomorphic-ws');
 
   ws = new ReconnectingWebSocket('ws://st-chat.shas.tel');
 
+  closeNotification = () => {
+    this.setState({showNotification: false})
+  }
+
+  checkSocketStatus = () => {
+     let status = this.ws.readyState;
+     console.log(status);
+     if (status === 3){
+       this.setState({connectionFlag: false});
+     } else if (status !== 3 && this.state.connectionFlag === false) {
+       this.setState({connectionFlag: true});
+     }
+   }
+  
   componentDidMount() {
     this.ws.onmessage = (message) => {
       const messageArray = JSON.parse(message.data).reverse();
       this.setState({messages: [...this.state.messages, messageArray]});
-      document.querySelector('#chatList').scrollTop = 99999;
+      this.setState({displayChat: true});
+      if (document.querySelector('#chatList') !== null) {
+        document.querySelector('#chatList').scrollTop = 99999;
+      } else this.setState({displayChat: true});
+      if (document.visibilityState === 'hidden' && messageArray[0] !== undefined) {
+        this.setState({showNotification: true, notificationMessageFrom: messageArray[0].from})
+      }
     }
   }
 
@@ -34,14 +58,35 @@ class MainPage extends Component {
 
   logout = () => {
     this.props.logout(null);
+    this.ws.close();
   }
 
   render() {
     return (
       <>
+        <Snackbar
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'right',
+          }}
+          open={this.state.showNotification}
+          autoHideDuration={2500}
+          //disableWindowBlurListener={false}
+          onClose={this.closeNotification}
+          message={<span id="message-id">You have a message from { this.state.notificationMessageFrom }</span>}
+          action={[
+            <IconButton
+              key="close"
+              color="inherit"
+              onClick={this.closeNotification}
+            >
+              x
+            </IconButton>
+          ]}
+        />
         <Header logout={this.logout}/>
-        <ChatList messages={this.state.messages}/> 
-        <ChatInput sendMessage={this.sendMessage} name={this.props.name}/>       
+        <ChatList messages={this.state.messages} displayProgress={this.state.displayChat}/>
+        <ChatInput sendMessage={this.sendMessage} name={this.props.name} connected={this.state.connectionFlag}/>       
       </>
     )
   }
