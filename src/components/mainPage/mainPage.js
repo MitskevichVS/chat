2437adfb18ce551780c896f4b25865536e832701
Mitskevich Victor <mv2.mx@yandex.ru>
2019-08-14 19:42:07 +0300

    refactor: code style

--- src/components/mainPage/mainPage.js
+++ src/components/mainPage/mainPage.js
@@ -1,6 +1,8 @@
+/* eslint-disable react/no-unused-state */
 import React, { Component } from 'react';
 import ReconnectingWebSocket from 'reconnecting-websocket';
 import store from 'store2';
+import PropTypes from 'prop-types';
 
 import Header from '../header/header';
 import ChatList from '../chatList/chatList';
@@ -9,68 +11,73 @@ import NotificationIcon from '../../images/NotificationIcon.png';
 
 
 class MainPage extends Component {
-  state = {
-    messages: [],
-    firstMessagesSlicedArray: [],
-    displayChat: false,
-    displayMessagesCount: 0,
-    connectionFlag: true,
-    scrollDownFlag: false,
-    scrollUpFlag: false,
-    userMessages: [],
-    userMessagesId: []
-  }
-
   ws = new ReconnectingWebSocket('ws://st-chat.shas.tel');
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      messages: [],
+      firstMessagesSlicedArray: [],
+      displayChat: false,
+      displayMessagesCount: 0,
+      connectionFlag: true,
+      scrollDownFlag: false,
+      scrollUpFlag: false,
+      userMessages: [],
+      userMessagesId: [],
+    };
+  }
+
   componentDidMount() {
-    if (!("Notification" in window)) {
-      alert("This browser does not support desktop notification");
+    if (!('Notification' in window)) {
+      // eslint-disable-next-line no-alert
+      alert('This browser does not support desktop notification');
     } else {
       Notification.requestPermission();
     }
 
 
-
     this.ws.onmessage = (message) => {
+      const { scrollDownFlag } = this.state;
       const messageArray = JSON.parse(message.data).reverse();
-      if (messageArray.length > 15){
+      if (messageArray.length > 15) {
         const size = 15;
-        let auxArray = [];
-        for (let i = 0; i < Math.ceil(messageArray.length/size); i+=1){
-          auxArray[i] = messageArray.slice((i*size), (i*size) + size);
-      }
-      this.setState({displayMessagesCount: auxArray.length - 1});
-      this.setState({firstMessagesSlicedArray: auxArray});
-      this.setInitialMessagesState(this.state.firstMessagesSlicedArray);
+        const auxArray = [];
+        for (let i = 0; i < Math.ceil(messageArray.length / size); i += 1) {
+          auxArray[i] = messageArray.slice((i * size), (i * size) + size);
+        }
+        this.setState({ displayMessagesCount: auxArray.length - 1 });
+        this.setState({ firstMessagesSlicedArray: auxArray });
+        // eslint-disable-next-line react/destructuring-assignment
+        this.setInitialMessagesState(this.state.firstMessagesSlicedArray);
       }
       this.checkUserMessages(messageArray);
-      if (messageArray.length < 15){
-        this.setState(prevState => ({
-          messages: [...prevState.messages, messageArray]
+      if (messageArray.length < 15) {
+        this.setState((prevState) => ({
+          messages: [...prevState.messages, messageArray],
         }));
       }
-      this.setState({displayChat: true});
-      
-      if (this.state.scrollDownFlag === true) {
+      this.setState({ displayChat: true });
+
+      if (scrollDownFlag === true) {
         const list = document.querySelector('#chatList');
         list.scrollTop = list.scrollHeight;
       }
 
-      
+
       if (document.querySelector('#chatList') === null) {
-        this.setState({displayChat: false});
+        this.setState({ displayChat: false });
       }
-      
+
       if (document.visibilityState === 'hidden' && messageArray[0] !== undefined) {
         this.notify(messageArray[0]);
       }
       this.checkListLength();
       if (store.get('userMessagesId').length > 0) {
         const userMessagesIdStore = store.get('userMessagesId');
-        this.setState({userMessagesId: userMessagesIdStore});
+        this.setState({ userMessagesId: userMessagesIdStore });
       }
-    }
+    };
   }
 
   componentWillUnmount() {
@@ -78,34 +85,36 @@ class MainPage extends Component {
   }
 
   setInitialMessagesState = (array) => {
-    let count = this.state.displayMessagesCount;
+    const { displayMessagesCount, firstMessagesSlicedArray } = this.state;
+    const count = displayMessagesCount;
     if (count < 0) return;
-    this.setState(prevState => ({
-      messages: [array[count], ...prevState.messages]
+    this.setState((prevState) => ({
+      messages: [array[count], ...prevState.messages],
     }));
-    this.setState({displayMessagesCount: count - 1});
-    this.setState({scrollUpFlag: false});
-    if (array[count].length < 10){
-      this.setInitialMessagesState(this.state.firstMessagesSlicedArray);
+    this.setState({ displayMessagesCount: count - 1 });
+    this.setState({ scrollUpFlag: false });
+    if (array[count].length < 10) {
+      this.setInitialMessagesState(firstMessagesSlicedArray);
     }
     document.querySelector('#chatList').scrollTop = 1500;
   }
 
   checkSocketStatus = () => {
-    let status = this.ws.readyState;
-     if (status === 3){
-       this.setState({connectionFlag: false});
-      } else if (status !== 3 && this.state.connectionFlag === false) {
-       this.setState({connectionFlag: true});
-      }
-   }
+    const { connectionFlag } = this.state;
+    const status = this.ws.readyState;
+    if (status === 3) {
+      this.setState({ connectionFlag: false });
+    } else if (status !== 3 && connectionFlag === false) {
+      this.setState({ connectionFlag: true });
+    }
+  }
 
   checkListLength = () => {
     const list = document.querySelector('#chatListUl');
     if (list === null) return;
     const listChildLength = list.childNodes.length;
     const listLengthStorage = store.get('listLength');
-    if (listChildLength >= listLengthStorage){
+    if (listChildLength >= listLengthStorage) {
       store.set('listLength', listChildLength);
     } else {
       store.set('listLength', 0);
@@ -115,79 +124,93 @@ class MainPage extends Component {
 
   checkUserMessages = (array) => {
     const { name } = this.props;
-    let userMessage = this.state.userMessages;
-    userMessage.forEach(item => {
-      let message = array.find(arrayObj => arrayObj.from === name && arrayObj.message === item.message);
-      if(message){
-        this.setState(prevState => ({
-         userMessagesId: [...prevState.userMessagesId, message.id]
-        }))
-        store.set('userMessagesId', this.state.userMessagesId);
+    const { userMessages, userMessagesId } = this.state;
+    const userMessage = userMessages;
+    userMessage.forEach((item) => {
+      // eslint-disable-next-line max-len
+      const message = array.find((arrayObj) => arrayObj.from === name && arrayObj.message === item.message);
+      if (message) {
+        this.setState((prevState) => ({
+          userMessagesId: [...prevState.userMessagesId, message.id],
+        }));
+        store.set('userMessagesId', userMessagesId);
       }
-    })
-   }
+    });
+  }
 
    sendMessage = (m) => {
-     let sendingData = {from: this.props.name, message: m};
-     this.setState(prevState => ({
-      userMessages: [...prevState.userMessages, sendingData]
-    }));
-    this.checkSocketStatus();
-    this.ws.send(JSON.stringify(sendingData));
-  }
-  
+     const { name } = this.props;
+     const sendingData = { from: name, message: m };
+     this.setState((prevState) => ({
+       userMessages: [...prevState.userMessages, sendingData],
+     }));
+     this.checkSocketStatus();
+     this.ws.send(JSON.stringify(sendingData));
+   }
+
   logout = () => {
-    this.props.logout(null);
+    const { logout } = this.props;
+    logout(null);
     this.ws.close();
   }
 
   handleScroll = (event) => {
-    let element = event.target;
+    const element = event.target;
+    const { firstMessagesSlicedArray } = this.state;
     if (element.scrollHeight - element.scrollTop === element.clientHeight) {
-      this.setState({scrollDownFlag: true});
+      this.setState({ scrollDownFlag: true });
     } else if (element.scrollTop === 0) {
-      this.setState({scrollUpFlag: true});
-      this.setInitialMessagesState(this.state.firstMessagesSlicedArray);
+      this.setState({ scrollUpFlag: true });
+      this.setInitialMessagesState(firstMessagesSlicedArray);
     } else {
-      this.setState({scrollDownFlag: false});
-      this.setState({scrollUpFlag: false});
+      this.setState({ scrollDownFlag: false });
+      this.setState({ scrollUpFlag: false });
     }
   }
-  
+
   notify = (messageFrom) => {
-    if (Notification.permission === "granted") {
-      let title = `You have a new message(s) from ${messageFrom.from}`;
-      let options = {
+    if (Notification.permission === 'granted') {
+      const title = `You have a new message(s) from ${messageFrom.from}`;
+      const options = {
         body: messageFrom.message,
         icon: NotificationIcon,
       };
-      let notification = new Notification(title, options);
-      notification.onclick = function(event) {
+      const notification = new Notification(title, options);
+      notification.onclick = (event) => {
         event.preventDefault();
         notification.close();
-      }
+      };
     }
   }
 
   render() {
+    const { name } = this.props;
+    const {
+      messages, connectionFlag, userMessagesId, displayChat,
+    } = this.state;
     return (
       <>
-        <Header logout={this.logout}/>
-        <ChatList 
-          messages={this.state.messages} 
-          displayProgress={this.state.displayChat}
+        <Header logout={this.logout} />
+        <ChatList
+          messages={messages}
+          displayProgress={displayChat}
           scroll={this.handleScroll}
-          userMessagesId={this.state.userMessagesId}
+          userMessagesId={userMessagesId}
         />
-        <ChatInput 
-          sendMessage={this.sendMessage} 
-          name={this.props.name} 
-          isConnected={this.state.connectionFlag}
+        <ChatInput
+          sendMessage={this.sendMessage}
+          name={name}
+          isConnected={connectionFlag}
           checkConnection={this.checkSocketStatus}
-        />       
+        />
       </>
-    )
+    );
   }
+}
+
+MainPage.propTypes = {
+  name: PropTypes.string.isRequired,
+  logout: PropTypes.func.isRequired,
 };
 
 export default MainPage;
